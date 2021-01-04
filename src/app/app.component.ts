39e9e8e43ed323ef308f4e48a745c13ac60f0d5c
MGYOSBEL <mgyosbel@gmail.com>
2021-01-04 09:58:30 +0100

    added swUpdate subscription behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HistoryService } from './video/services/history.service';
 import { FavoriteService } from './video/services/favorite.service';
 import { VideoService } from './video/services/video.service';
 import { SearchResult } from './search/Models/SearchResult.model';
+import { SwUpdate } from '@angular/service-worker';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'youtube-library';
 
   list$;
   sideDrawerTitle?: string;
 
+  unsubscribe$ = new Subject<void>();
+
   constructor(
     private historyService: HistoryService,
     private favoriteService: FavoriteService,
-    private videoService: VideoService
+    private videoService: VideoService,
+    private swUpdate: SwUpdate
   ) {
     this.list$ = this.historyService.history$;
+  }
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.pipe(
+        takeUntil(this.unsubscribe$)
+      ).subscribe(() => {
+        if (confirm(('New version is available. Load New Version?'))) {
+          window.location.reload();
+        }
+      }
+      );
+    }
   }
 
   viewHistory(): void {
@@ -35,5 +54,10 @@ export class AppComponent {
 
   playVideo(video: SearchResult): void {
     this.videoService.play(video);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
